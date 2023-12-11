702aff1cf13afaced383270d81de762ccde773a1
devzeshan <zeshan.ahmed@programmer.net>
2023-12-11 00:07:37 -0500

    Some changes.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+    const arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push(str);
+    }
+    return newArr = arr.join("");
+}
+}
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    tofillStr = '';
+    for (var i = str.length(); i => 0; i--) {
+        tofillStr =+ str[i];
+    }
+    return tofillStr;
 
 };
+
+console.log(reverseString('hi'));
 
 // Do not edit below this line
 module.exports = reverseString;
